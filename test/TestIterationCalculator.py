68290e33b4d6d2d3c0596d3d17a1c74f401e6a39
mpm <mpmiszczyk@gmail.com>
2013-05-04 01:31:59 +0200

    Skipping test we don't know how to write.

--- test/TestIterationCalculator.py
+++ test/TestIterationCalculator.py
@@ -1,14 +1,23 @@
 import unittest
+from unittest.case import skip
 from Calculator import calculate_next_state
 from CelluralAutoma import Cells, Rule
 
 
 class TestCellularCalculation( unittest.TestCase ):
+   @skip( "to be implemented" )
    def test_creating_interface (self):
       cells = Cells( )
       rule = Rule( )
 
+      # for each cell in cells
+      # pass cell naigbours to rule
+      # rule returns one cell stage
+      #
+
       new_cells = calculate_next_state( cells, rule )
+
+      self.fail( "?no lower logic to test?" )
 
 
 if __name__ == '__main__':
