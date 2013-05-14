2075d2440fc891b76fb1a1a1d93bd31e8d5a5da0
mpm <mpmiszczyk@gmail.com>
2013-05-14 13:01:53 +0200

    Rest of example copied from tutorial.

--- src/exTry/exTry.py
+++ src/exTry/exTry.py
@@ -11,9 +11,26 @@ main_frame.columnconfigure( 0, weight=1 )
 main_frame.rowconfigure( 0, weight=1 )
 
 feet = tkinter.StringVar( )
-meters = tkinter.StringVar( )
 
 feet_entry = ttk.Entry( main_frame, width=7, textvariable=feet )
 feet_entry.grid( column=2, row=1, sticky=(W, E) )
+
+meters = tkinter.StringVar( )
+ttk.Label( main_frame, textvariable=meters ).grid( column=2, row=2, sticky=W )
+
+
+def calculate (*args):
+   try:
+      value = float( feet.get( ) )
+      meters.set( (0.3048 * value * 10000.0 + 0.5) / 10000.0 )
+   except ValueError:
+      pass
+
+
+ttk.Button( main_frame, text="Calculate", command=calculate ).grid( column=2, row=3, sticky=(W, E) )
+
+ttk.Label( main_frame, text="feet" ).grid( column=3, row=1, sticky=W )
+ttk.Label( main_frame, text="is equivalent to" ).grid( column=1, row=2, sticky=E )
+ttk.Label( main_frame, text="meters" ).grid( column=3, row=2, sticky=W )
 
 top.mainloop( )
