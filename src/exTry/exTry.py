e8ff20449ebacd33adadc2e315853c20081e347b
mpm <mpmiszczyk@gmail.com>
2013-05-14 12:54:14 +0200

    First entry field

--- src/exTry/exTry.py
+++ src/exTry/exTry.py
@@ -1,8 +1,19 @@
 import tkinter
-from tkinter import ttk
+from tkinter import ttk, N, W, E, S
 
 
 top = tkinter.Tk( )
-ttk.Button( top, text="Hallo world" ).grid( )
+top.title( "feet to meters" )
+
+main_frame = ttk.Frame( top, padding="3 3 12 12" )
+main_frame.grid( column=0, row=0, sticky=(N, W, E, S) )
+main_frame.columnconfigure( 0, weight=1 )
+main_frame.rowconfigure( 0, weight=1 )
+
+feet = tkinter.StringVar( )
+meters = tkinter.StringVar( )
+
+feet_entry = ttk.Entry( main_frame, width=7, textvariable=feet )
+feet_entry.grid( column=2, row=1, sticky=(W, E) )
 
 top.mainloop( )
