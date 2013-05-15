c66c4fb12b112fa4d76ab103d3bab325d507a253
mpm <mpmiszczyk@gmail.com>
2013-05-15 16:35:15 +0200

    Added drawn buttons handling color change.

--- src/tryCanvas.py
+++ src/tryCanvas.py
@@ -9,9 +9,17 @@ def xy (event):
    lastx, lasty = event.x, event.y
 
 
+color = "black"
+
+
+def setColor (newcolor):
+   global color
+   color = newcolor
+
+
 def addLine (event):
    global lastx, lasty
-   canvas.create_line( lastx, lasty, event.x, event.y, fill='red', width=3 )
+   canvas.create_line( (lastx, lasty, event.x, event.y), fill=color )
    lastx, lasty = event.x, event.y
 
 
@@ -23,5 +31,12 @@ canvas = Canvas( root )
 canvas.grid( column=0, row=0, sticky=(N, W, E, S) )
 canvas.bind( "<Button-1>", xy )
 canvas.bind( "<B1-Motion>", addLine )
+
+id = canvas.create_rectangle( (10, 10, 30, 30), fill="red" )
+canvas.tag_bind( id, "<Button-1>", lambda x: setColor( "red" ) )
+id = canvas.create_rectangle( (10, 35, 30, 55), fill="blue" )
+canvas.tag_bind( id, "<Button-1>", lambda x: setColor( "blue" ) )
+id = canvas.create_rectangle( (10, 60, 30, 80), fill="black" )
+canvas.tag_bind( id, "<Button-1>", lambda x: setColor( "black" ) )
 
 root.mainloop( )
